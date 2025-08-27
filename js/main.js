89cacad91b48e7d752b444993078ae2f13c70a55
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-08-27 17:56:56 -0500

    Proyecto simulador de inversión terminado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-function simulador(){
-    let cNombres = document.getElementById('nombres_apellidos');
-    let cTelefono = document.getElementById('telefono');
-    let cEmail = document.getElementById('email');
-    let cMonto = document.getElementById('monto_invertido');
-    let cOpciones = document.getElementById('tiempo_inversion');
+function simulador() {
+
+    // VARIABLES QUE CAPTURAN LOS VALORES DE LOS CAMPOS
+    let cNombres = document.getElementById('nombres_apellidos').value;
+    let cTelefono = document.getElementById('telefono').value;
+    let cEmail = document.getElementById('email').value;
+    let cMonto = document.getElementById('monto_invertido').value;
+    let cOpciones = document.getElementById('tiempo_inversion').value;
+
+    // CONSTANTES QUE CAPTURAN LAS ETIQUETAS SPAN EN LAS QUE SE MUESTRAN LOS VALORES POR ESO LA S DE SHOW
+    const sTiempo = document.getElementById('sTiempo');
+    const sInteres = document.getElementById('sInteres');
+    const sTotal = document.getElementById('sTotal');
+    const sGanancia = document.getElementById('sGanancia');
+    const sNombres = document.getElementById('sNombres');
+    const sCorreo = document.getElementById('sCorreo');
+
     
+
     // CAPTURAMOS EL ID DE LOS CAMPOS EN UNA CONSTANTE
     const contPre = document.getElementById('pre-simulation');
     const contPost = document.getElementById('post-simulation');
@@ -12,8 +24,44 @@ function simulador(){
     // HACEMOS LA ACCIÓN DE QUITAR LA CLASE DISABLE DEL CONTPOST PARA PONERSELA AL CONTPRE
     contPost.classList.remove('disabled');
     contPre.classList.add('disabled');
+
+    // VARIABLES PARA REALIZAR LOS CÁLCULOS 
+    let ganancia = 0;
+    let total = 0;
+
+    switch (cOpciones) {
+        case '1':
+            // En este case realizaremos el cálculo si el tiempo corresponde a un año
+            ganancia = (cMonto * 0.008) * 12;
+            total = parseInt(cMonto) + parseInt(ganancia);
+            sTiempo.textContent = '12 meses';
+            sInteres.textContent = '0.8%';
+            break;
+        
+        case '2':
+            // En este case realizaremos el cálculo si el tiempo corresponde a dos años
+            ganancia = (cMonto * 0.013) * 24;
+            total = parseInt(cMonto) + parseInt(ganancia);
+            sTiempo.textContent = '24 meses';
+            sInteres.textContent = '1.3%';
+            break;
+        
+        case '3':
+            // En este case realizaremos el cálculo si el tiempo corresponde a tres años
+            ganancia = (cMonto * 0.017) * 36
+            total = parseInt(cMonto) + parseInt(ganancia);
+            sTiempo.textContent = '36 meses';
+            sInteres.textContent = '1.7%';
+            break;      
+    }
+
+    sTotal.textContent = total;
+    sGanancia.textContent = ganancia;
+    sNombres.textContent = cNombres;
+    sCorreo.textContent = cEmail;
+    
 }
 
-document.addEventListener('DOMContentLoaded' , function (){
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('calcular').addEventListener('click', simulador);
 });
